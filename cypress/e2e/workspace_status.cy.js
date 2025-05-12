380797197766d9a43d6aa38080641ed444784da6
MiraVoronina <voroninaslava23@gmail.com>
2025-05-12 21:05:48 +0700

    4 commit
    смена статуса заявки

---
+++ cypress/e2e/workspace_status.cy.js
@@ -0,0 +1,19 @@
+describe('Смена статуса потребности работодателем', () => {
+    beforeEach(() => {
+        cy.visit('https://dev.profteam.su/login');
+        cy.get('input[type="text"]').first().type('testerEmployer');
+        cy.get('input[type="password"]').type('Password1');
+        cy.contains('button', 'Войти').click();
+        cy.url().should('include', '/account');
+    });
+
+    it('Публикация потребности', () => {
+        cy.visit('https://dev.profteam.su/account/needs');
+        cy.wait(1500); 
+
+        cy.contains('Опубликовать').scrollIntoView().should('exist');
+        cy.contains('Опубликовать').click({ force: true });
+
+        cy.wait(1000);
+    });
+});
